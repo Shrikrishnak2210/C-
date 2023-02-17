137469d5568d863695576fe657e99b371609c272
Shrikrishna Keeliputti <73592987+Shrikrishnak2210@users.noreply.github.com>
2023-02-18 00:27:54 +0530

    first commit

--- C++/lenptr.c
+++ C++/lenptr.c
@@ -1,17 +1,19 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 int main()
 {
     int length;
-    char text[20],*text_ptr;
+    char text[20], *text_ptr;
     text_ptr = text;
     printf("Enter text \n");
     gets(text);
-    while(*text_ptr != '\0')
+    while (*text_ptr != '\0')
     {
         putchar(*text_ptr++);
     }
     length = text_ptr - text;
-    printf("Length of the text is = %d",length);
+    printf("Length of the text is = %d", length);
     return 0;
+
+    // Yello
 }
